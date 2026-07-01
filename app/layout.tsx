81d31d1cb1ec52d90dc007d2c63eb28fab1afb99
ishallym-pr-alt <ishallym-pr@hanmail.net>
2026-07-01 14:18:40 +0900

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "한림병원 직원용 포탈",
@@ -21,6 +22,7 @@ export default function RootLayout({
       </head>
       <body className="bg-gray-50 overflow-x-hidden">
         {children}
+        <Analytics />
       </body>
     </html>
   );
